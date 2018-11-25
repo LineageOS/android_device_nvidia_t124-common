6b21265d347c08c5aa3bc20eeaa3b113130c1835
Aaron Kling <webgeek1234@gmail.com>
2018-11-24 23:48:25 -0600

    Add system properties
    Based on stock shieldtablets build.prop
    
    Change-Id: Idc91a5232d87fff0d51f95efe94acab2583f1231

---
+++ system_prop.mk
@@ -0,0 +1,16 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.resampler.quality=7 \
+    af.fast_downmix=1 \
+    media.aac_51_output_enabled=1 \
+    ro.af.client_heap_size_kbyte=8192 \
+    ro.audio.usb.period_us=5000
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610 \
+    persist.tegra.compositor=glcomposer
+
+# Input
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.input.noresample=1
