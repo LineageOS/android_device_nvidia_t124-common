764751bd5e283881396df4b4efa1509cd87b8c66
Aaron Kling <webgeek1234@gmail.com>
2022-01-04 01:41:44 -0600

    Add intrinsics shim to mplmpu
    Change-Id: Idb0de530d5e3efe1d1e88f34cad1a33c696a554d

--- extract/patches.sh
+++ extract/patches.sh
@@ -31,6 +31,7 @@ function patch_t124_intrinsics() {
   sed -i 's/libm.so/libw.so/g' ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/libglcore.so
   sed -i 's/libm.so/libw.so/g' ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/libnvglsi.so
   sed -i 's/libm.so/libw.so/g' ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/libnvrmapi_tegra.so
+  sed -i 's/libm.so/libw.so/g' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
 
   sed -i 's/__aeabi_ul2d/s_aeabi_ul2d/'         ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/egl/libEGL_tegra.so
   sed -i 's/__aeabi_ul2f/s_aeabi_ul2f/'         ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/egl/libEGL_tegra.so
@@ -60,6 +61,37 @@ function patch_t124_intrinsics() {
   sed -i 's/__aeabi_uldivmod/s_aeabi_uldivmod/' ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/libnvglsi.so
 
   sed -i 's/__aeabi_uldivmod/s_aeabi_uldivmod/' ${LINEAGE_ROOT}/${OUTDIR}/t124/nvgpu/lib/libnvrmapi_tegra.so
+
+  sed -i 's/__aeabi_d2f/s_aeabi_d2f/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_d2iz/s_aeabi_d2iz/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_d2lz/s_aeabi_d2lz/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dadd/s_aeabi_dadd/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dcmpeq/s_aeabi_dcmpeq/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dcmpge/s_aeabi_dcmpge/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dcmpgt/s_aeabi_dcmpgt/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dcmple/s_aeabi_dcmple/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dcmplt/s_aeabi_dcmplt/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_ddiv/s_aeabi_ddiv/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dmul/s_aeabi_dmul/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_dsub/s_aeabi_dsub/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_f2d/s_aeabi_f2d/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_f2iz/s_aeabi_f2iz/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_f2lz/s_aeabi_f2lz/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fadd/s_aeabi_fadd/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fcmpeq/s_aeabi_fcmpeq/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fcmpge/s_aeabi_fcmpge/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fcmpgt/s_aeabi_fcmpgt/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fcmple/s_aeabi_fcmple/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fcmplt/s_aeabi_fcmplt/' ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fdiv/s_aeabi_fdiv/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fmul/s_aeabi_fmul/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_fsub/s_aeabi_fsub/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_i2d/s_aeabi_i2d/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_i2f/s_aeabi_i2f/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_idiv/s_aeabi_idiv/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_l2d/s_aeabi_l2d/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_l2f/s_aeabi_l2f/'       ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
+  sed -i 's/__aeabi_ui2f/s_aeabi_ui2f/'     ${LINEAGE_ROOT}/${OUTDIR}/t124/sensors/lib/libmplmpu.so
 }
 
 patch_t124_glcore;
