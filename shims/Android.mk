9db422f53a33e55886071cf49995d003dbac3934
Aaron Kling <webgeek1234@gmail.com>
2022-03-03 17:49:14 -0600

    Add nvmm shim to GraphicBuffer constructor
    The variant in use no longer exists. This shim discards the
    parameters.
    
    Change-Id: I9195d4329848eceb98dbe71d3ff631accc4c2da0

--- shims/Android.mk
+++ shims/Android.mk
@@ -17,9 +17,9 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE           := libnvmm_shim
-LOCAL_SRC_FILES        := nvmm_connect.cpp
+LOCAL_SRC_FILES        := nvmm_shims.cpp
 LOCAL_MULTILIB         := 32
-LOCAL_SHARED_LIBRARIES := libstagefright
+LOCAL_SHARED_LIBRARIES := libstagefright libui
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_SHARED_LIBRARY)
 
